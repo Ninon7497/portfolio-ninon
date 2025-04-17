90a979db58eb08d66dcdf7840364eb8428d0dadd
Ninon Roche <rocheninon74@gmail.com>
2025-04-17 14:49:23 +0200

    first commit

--- src/components/HeroSection/HeroSection.vue
+++ src/components/HeroSection/HeroSection.vue
@@ -1,23 +1,33 @@
 <script setup>
-    import HeroContent from '@components/HeroSection/HeroContent.vue'
-    import HeroImage from '@components/HeroSection/HeroImage.vue'
+import HeroContent from '@components/HeroSection/HeroContent.vue'
+import HeroImage from '@components/HeroSection/HeroImage.vue'
 </script>
+
 <template>
-    <section class="min-h-screen relative z-1 overflow-hidden" ref="section" id="home">
-        <div class="px-16 flex justify-center">
-            <article class="grid grid-cols-1 lg:grid-cols-2 gap-8 items-center h-ful py-20">
-                <header class="flex flex-col justify-center text-center md:text-left">
-                    <HeroContent/>
-                    <a href="https://www.lecole-cci.fr/" target="_blank" class="w-[200px]"><img src="/ecole.png" alt="logo école" class="w-[200px] h-[200px] rounded-full mt-20 hover:cursor-pointer"></a> 
-                </header>
-                <figure class="relative flex items-center justify-center" >
-                    <HeroImage/>
-                </figure>
-            </article>
+  <section class="min-h-screen relative z-1 overflow-hidden" ref="section" id="home">
+    <div class="px-4 sm:px-8 md:px-12 lg:px-16 flex justify-center">
+      <article class="grid grid-cols-1 lg:grid-cols-2 gap-8 items-center h-full py-20">
+        <!-- Contenu principal -->
+        <header class="flex flex-col justify-center text-center md:text-left">
+          <HeroContent />
+          <a
+            href="https://www.lecole-cci.fr/"
+            target="_blank"
+            class="w-[200px]"
+          >
+            <img
+              src="/ecole.png"
+              alt="logo école"
+              class="w-[200px] h-[200px] rounded-full mt-20 hover:cursor-pointer"
+            />
+          </a>
+        </header>
 
-        </div>
-
-    </section>
-
-
+        <!-- Image (affichée uniquement à partir de lg) -->
+        <figure class="relative items-center justify-center hidden lg:flex">
+          <HeroImage />
+        </figure>
+      </article>
+    </div>
+  </section>
 </template>
