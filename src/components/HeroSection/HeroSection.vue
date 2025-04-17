cc272bf64cae4aa2517b31ea01d811d26808e5fb
Ninon Roche <rocheninon74@gmail.com>
2025-04-17 14:54:52 +0200

    commit

--- src/components/HeroSection/HeroSection.vue
+++ src/components/HeroSection/HeroSection.vue
@@ -5,15 +5,15 @@ import HeroImage from '@components/HeroSection/HeroImage.vue'
 
 <template>
   <section class="min-h-screen relative z-1 overflow-hidden" ref="section" id="home">
-    <div class="px-4 sm:px-8 md:px-12 lg:px-16 flex justify-center">
+    <div class="px-16 flex justify-center">
       <article class="grid grid-cols-1 lg:grid-cols-2 gap-8 items-center h-full py-20">
-        <!-- Contenu principal -->
         <header class="flex flex-col justify-center text-center md:text-left">
           <HeroContent />
+          <!-- Ce bloc sera caché sur les écrans inférieurs à 'lg' -->
           <a
             href="https://www.lecole-cci.fr/"
             target="_blank"
-            class="w-[200px]"
+            class="w-[200px] hidden lg:block"
           >
             <img
               src="/ecole.png"
@@ -23,8 +23,7 @@ import HeroImage from '@components/HeroSection/HeroImage.vue'
           </a>
         </header>
 
-        <!-- Image (affichée uniquement à partir de lg) -->
-        <figure class="relative items-center justify-center hidden lg:flex">
+        <figure class="relative flex items-center justify-center">
           <HeroImage />
         </figure>
       </article>
